85b75fc8c521dfec2555d5a2382bf83393a521e5
xls0Jacker <2741486550@qq.com>
2022-08-23 12:16:55 +0800

    2022/8/23 Web前端设计 电子教程 js 至4.3

---
+++ code/web/book online js/4/4.1/homework/in-4.js
@@ -0,0 +1,8 @@
+describe("multiplyNumeric", function() {
+    it("width is double", function() {
+        assert.equal(getWidth(),400);
+    });
+    it("for non-integer n the result is NaN", function() {
+        assert.equal(getHeight(),600);
+    });
+});
